c0f07835ec99b0db7e16196423e8ae4221296145
Burntroll <francisco.silva.ru@gmail.com>
2022-07-24 16:57:53 +0300

    Finalização da atividade sobre Reduce

--- 07 - Map-Filter-Reduce/reduce.js
+++ 07 - Map-Filter-Reduce/reduce.js
@@ -1,3 +1,5 @@
+/* ATIVIDADE 1 */
+
 function somaNumeros(array) {
     return array.reduce(function(prev, current) {
         console.log({prev});
@@ -9,3 +11,33 @@ function somaNumeros(array) {
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 console.log(somaNumeros(array));
+
+/* ATIVIDADE 2 */
+
+const lista = [
+    {
+        name: "washing powder",
+        price: 30
+    },
+    {
+        name: "cereal",
+        price: 12
+    },
+    {
+        name: "towel",
+        price: 30
+    }
+];
+
+let saldoDisponivel = 100;
+
+function calculaSaldo(saldoDisponivel, lista) {
+    return lista.reduce(function(prev, current, index) {
+        console.log("rodada", index + 1);
+        console.log({ prev });
+        console.log({ current });
+        return prev - current.price
+    }, saldoDisponivel)
+}
+
+console.log(calculaSaldo(saldoDisponivel, lista));
