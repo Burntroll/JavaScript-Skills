ce9993c1facdf8422cb7744d1d64d9c539698d81
Burntroll <francisco.silva.ru@gmail.com>
2022-07-25 13:20:58 +0300

    Completo a atividade de módulos

---
+++ 11 - Módulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Francisco", 26));
+console.log(mostraCidade("Francisco", "Fortaleza"));
+console.log(mostraHobby("Francisco", "study"));
